ef4c689f8f6b5671d6dbc19f4171eb710c60e689
Astrophel66 <sarinanepal30@gmail.com>
2026-04-05 17:22:09 +0545

    model creation of completion and dailyact app

--- completion/models.py
+++ completion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from account.models import User
+from dailyact.models import DailyAct
 # Create your models here.
+class Completion(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    act=models.ForeignKey(DailyAct, on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.user
--- dailyact/models.py
+++ dailyact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class DailyAct(models.Model):
+    act =models.CharField(max_length = 100)
+    date=models.DateField(unique=True)
+    
+    
+    def __str__(self):
+        return self.act
